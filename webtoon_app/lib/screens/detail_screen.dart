d33311e2e9b229582337e1bb237e3f176d363418
haagmin5382 <81218474+haagmin5382@users.noreply.github.com>
2023-08-14 17:02:04 +0900

    feat : add favorite webtoon func

--- webtoon_app/lib/screens/detail_screen.dart
+++ webtoon_app/lib/screens/detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:webtoon_app/models/webtoon_detail_model.dart';
 import 'package:webtoon_app/models/webtoon_episode_model.dart';
 import 'package:webtoon_app/services/api_service.dart';
@@ -16,15 +17,55 @@ class DetailScreen extends StatefulWidget {
 
 class _DetailScreenState extends State<DetailScreen> {
   late Future<WebtoonDetailModel> webtoon = ApiService().getToonById(widget.id);
-
   late Future<List<WebtoonEpisodeModel>> episodes =
       ApiService().getEpisodesById(widget.id);
+  late SharedPreferences prefs;
+  bool isLiked = false;
   // constructor에서 widget이 참조될 수 없기 때문에 사용할 수 없다.
   // 그래서 나중에 define할 거라고 적어주어야 한다. => late
+  Future initPrefs() async {
+    prefs = await SharedPreferences.getInstance(); // 저장소에 커넥션이 생김
+    final likedToons = prefs.getStringList("likedToons");
+
+    if (likedToons != null) {
+      if (likedToons.contains(widget.id) == true) {
+        // 사용자가 지금 보고 있는 webtoon의 ID가 likedToons 안에 있는지 확인한다
+        // 있다면 사용자가 웹툰에 좋아요를 누른 적 이 있다는 것이다.
+        setState(() {
+          isLiked = true;
+        }); // setState를 해줘야 상태에 맞는 UI를 그려준다.
+      }
+    } else {
+      prefs.setStringList("likedToons", []);
+      // likedToons 없을 때 key, value 세팅
+      // likedToons는 리스트로 오기 때문에 빈배열로 세팅한다.
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPrefs();
+  }
+
+  onHeartTap() async {
+    // initState 실행된 이후라 initPrefs도 실행되었기 때문에 likedToons는 빈배열이라도 만들어졌다.
+    final likedToons = prefs.getStringList("likedToons");
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.id); // 이미 좋아요 눌러져있다면 likedToons배열에서 삭제
+      } else {
+        likedToons.add(widget.id);
+      }
+      await prefs.setStringList("likedToons", likedToons);
+      setState(() {
+        isLiked = !isLiked;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    print(episodes);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -38,8 +79,10 @@ class _DetailScreenState extends State<DetailScreen> {
         foregroundColor: Colors.green,
         actions: [
           IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.favorite_outline_outlined))
+              onPressed: onHeartTap,
+              icon: isLiked
+                  ? const Icon(Icons.favorite)
+                  : const Icon(Icons.favorite_outline_outlined))
         ],
       ),
       body: SingleChildScrollView(
